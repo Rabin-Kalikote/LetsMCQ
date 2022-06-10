1c2639fa5a55f2e2645774d7d98ef4a084133b60
Rabin-Kalikote <rabinkalikote33@gmail.com>
2022-06-10 19:09:30 +0545

    Let's MCQ

--- config/application.rb
+++ config/application.rb
@@ -12,6 +12,7 @@ module Chatter
     config.load_defaults 5.2
 	config.time_zone = 'Eastern Time (US & Canada)'
 	config.active_record.default_timezone = :local
+  config.assets.initialize_on_precompile = false
   ActionCable.server.config.disable_request_forgery_protection = true
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
