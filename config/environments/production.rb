e577577939bf4fa3fe06640e349882740a7a855b
Rabin-Kalikote <rabinkalikote33@gmail.com>
2022-06-10 21:23:46 +0545

    what

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -3,8 +3,8 @@ Rails.application.configure do
 
   # Code is not reloaded between requests.
   config.cache_classes = true
-  config.action_cable.url = ['wss://deanout.online/cable']
-  config.action_cable.allowed_request_origins = ['https://deanout.online']
+  config.action_cable.url = ['wss://letsmcq.herokuapp.com/cable']
+  config.action_cable.allowed_request_origins = ['https://letsmcq.herokuapp.com']
   config.action_cable.disable_request_forgery_protection = true
   # Eager load code on boot. This eager loads most of Rails and
   # your application in memory, allowing both threaded web servers
--- db/migrate/20180418021054_add_last_sign_out_at_to_users.rb
+++
@@ -1,5 +0,0 @@
-class AddLastSignOutAtToUsers < ActiveRecord::Migration[5.2]
-  def change
-    add_column :users, :last_sign_out_at, :datetime
-  end
-end
